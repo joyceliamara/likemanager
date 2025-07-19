fa55ffe49323ea5f0c03c31231f98b2896a26f57
joycemarinheiro <joycemarinheiro.jobs@gmail.com>
2025-07-19 10:20:32 -0300

    feat: Estruturação layout e navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Layout } from "./components/Layouts";
+
 export default function Home() {
   return (
-    <div className="bg-red-500">Bem vindo ao Gerenciador de Curtidas</div>
+    <Layout />
   );
 }
